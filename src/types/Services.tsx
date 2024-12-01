ed48476e72cc7cb393996bbd9deb258a67cef730
spl3g <spleefer6@yandex.ru>
2024-12-02 01:07:23 +0300

    feat: add services type

---
+++ src/types/Services.tsx
@@ -0,0 +1,36 @@
+import { Card } from "@consta/uikit/Card";
+import { Text } from "@consta/uikit/Text";
+import { Avatar } from "@consta/uikit/Avatar";
+
+export type Service = {
+    name: string;
+    description: string;
+    image: string;
+    createdAt: string;
+};
+
+export const ServicesListData: React.FC<Service> = function ({
+    name,
+    description,
+    image,
+    createdAt,
+}) {
+    return (
+        <Card verticalSpace="m" horizontalSpace="m">
+            <Text weight="semibold">{name}</Text>
+            <hr />
+            <div style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
+                <Avatar
+                    url={image}
+                    name="service-image"
+                    size="l"
+                    style={{ flexShrink: 0 }}
+                />
+                <Text>{description}</Text>
+            </div>
+            <Text size="xs" align="right" view="ghost">
+                {createdAt}
+            </Text>
+        </Card>
+    );
+};
