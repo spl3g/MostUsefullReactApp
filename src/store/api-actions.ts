33fd5f61573f3dde836c03796b88bca489c9ee32
spl3g <spleefer6@yandex.ru>
2024-12-02 00:02:59 +0300

    feat: add function return type

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,7 +1,8 @@
 import { URLs } from "../const";
 import axios from "axios";
+import { News } from "../types/News";
 
-export const getNewsListAction = async function () {
+export const getNewsListAction = async (): Promise<News[]> => {
     let response = await axios.get(URLs.news);
     return await response.data;
 };
