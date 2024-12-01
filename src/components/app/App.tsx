95ee32725a79d3be109a978850cad1dbf754ecb4
spl3g <spleefer6@yandex.ru>
2024-12-01 21:37:33 +0300

    feat: use default layout in "/" path

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,19 +2,17 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { presetGpnDefault, Theme } from "@consta/uikit/Theme";
 import { Responses404 } from "@consta/uikit/Responses404";
-import Header from "../header/Header.tsx";
-import React from "react";
-import MainPage from "../../pages/main-page/MainPage.tsx";
+import MainPage from "../../pages/main-page/MainPage";
+import DefaultLayout from "../../layouts/default/DefaultLayout";
 
 function App() {
     return (
         <Theme preset={presetGpnDefault}>
             <BrowserRouter>
-                <Header />
                 <Routes>
-                    <Route path="/">
+                    <Route path="/" element={<DefaultLayout />}>
                         <Route index element={<MainPage />}></Route>
-                        {/*<Route path="services" element={<MainPage />}></Route>*/}
+                        <Route path="services" element={<MainPage />}></Route>
                         <Route path="login" element={<MainPage />}></Route>
                     </Route>
                     <Route path="*" element={<Responses404 />}></Route>
