2b11074d2c01636058bd78031249207e40ae75a2
spl3g <spleefer6@yandex.ru>
2024-12-02 01:06:53 +0300

    feat: implement get services function

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,8 +1,14 @@
 import { URLs } from "../const";
 import axios from "axios";
 import { News } from "../types/News";
+import { Service } from "../types/Services";
 
 export const getNewsListAction = async (): Promise<News[]> => {
     let response = await axios.get(URLs.news);
     return await response.data;
+};
+
+export const getServicesListAction = async (): Promise<Service[]> => {
+    let response = await axios.get(URLs.services);
+    return response.data;
 };
