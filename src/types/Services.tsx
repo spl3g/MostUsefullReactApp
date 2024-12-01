670c0d6a25a2d6791d5489abcb64501ef8ca51cb
Ozornin Matvey <58591608+spl3g@users.noreply.github.com>
2024-12-02 01:35:02 +0300

    PR feat: add links to services cards (#3)
    * feat: add id to the service type
    
    * feat: make text clickable

--- src/types/Services.tsx
+++ src/types/Services.tsx
@@ -1,8 +1,10 @@
 import { Card } from "@consta/uikit/Card";
 import { Text } from "@consta/uikit/Text";
 import { Avatar } from "@consta/uikit/Avatar";
+import { useNavigate } from "react-router-dom";
 
 export type Service = {
+    id: number;
     name: string;
     description: string;
     image: string;
@@ -10,14 +12,22 @@ export type Service = {
 };
 
 export const ServicesListData: React.FC<Service> = function ({
+    id,
     name,
     description,
     image,
     createdAt,
 }) {
+    const navigate = useNavigate();
     return (
         <Card verticalSpace="m" horizontalSpace="m">
-            <Text weight="semibold">{name}</Text>
+            <Text
+                weight="semibold"
+                style={{ cursor: "pointer" }}
+                onClick={() => navigate(`/services/${id}`)}
+            >
+                {name}
+            </Text>
             <hr />
             <div style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
                 <Avatar
